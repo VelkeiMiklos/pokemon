04b47e1b9062d5979e82d180acf724ef078a48f8
VelkeiMiklos <velkei18@gmail.com>
2017-10-17 17:50:12 +0200

    Initial Commit

---
+++ PokemonPokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokemonPokedex
+//
+//  Created by Velkei Miklós on 2017. 10. 17..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
