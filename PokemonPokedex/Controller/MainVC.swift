5f405de156c953d25e2e3c527ccbaa20b3466eee
VelkeiMiklos <velkei18@gmail.com>
2017-10-19 19:21:31 +0200

    search bar + music

--- PokemonPokedex/Controller/MainVC.swift
+++ PokemonPokedex/Controller/MainVC.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+import AVFoundation
+class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,UISearchBarDelegate{
     
     //Outlets
     @IBOutlet weak var collectionView: UICollectionView!
@@ -17,13 +17,18 @@ class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     //Variables
     var pokemon: Pokemon!
     var pokemons = [Pokemon]()
+    var filteredPokemon = [Pokemon]()
+    var searchActive : Bool = false
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.delegate = self
         collectionView.dataSource = self
+        searchBar.delegate = self
         parseCSV()
+        initMusic()
     }
-    
     
     //Functions
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -31,20 +36,56 @@ class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pokemons.count
+        if searchActive{
+            return filteredPokemon.count
+        }else{
+            return pokemons.count
+        }
+        
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "pokemonCell", for: indexPath) as? PokemonCell{
             
             //let pokemon = Pokemon(pokemonName: "Pokemon", pokemonId: indexPath.row)
-            let pokemon = pokemons[indexPath.row]
-            cell.configureCell(pokemon: pokemon)
+            //let pokemon = pokemons[indexPath.row]
+            let poke: Pokemon!
+            if searchActive{
+                poke = filteredPokemon[indexPath.row]
+            }else{
+                poke = pokemons[indexPath.row]
+            }
+            
+            cell.configureCell(pokemon: poke)
             return cell
         }else{
             return PokemonCell()
         }
     }    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        //search happened
+        filteredPokemon = pokemons.filter{ $0.pokemonName.lowercased().contains( searchBar.text!.lowercased()) }
+        
+        if filteredPokemon.count != 0{
+            searchActive = true
+        }else{
+            searchActive = false
+        }
+        self.collectionView.reloadData()
+    }
     
+    //Actions
+    @IBAction func musicBtnPressed(_ sender: UIButton) {
+        if audioPlayer.isPlaying{
+            audioPlayer.stop()
+            sender.alpha = 0.5
+            
+        }else{
+            audioPlayer.play()
+            sender.alpha = 1
+        }
+    }
+    
+    //Init methods
     func parseCSV(){
         let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
         do{
@@ -62,5 +103,16 @@ class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
         }
     }
     
+    func initMusic(){
+        let url = Bundle.main.url(forResource: "music", withExtension: "mp3")!
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        } catch {
+            print("Cannot play the file")
+        }
+    }
 }
 
---
+++ PokemonPokedex/Controller/PokemonDetailVC.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonDetailVC.swift
+//  PokemonPokedex
+//
+//  Created by Velkei Miklós on 2017. 10. 19..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
